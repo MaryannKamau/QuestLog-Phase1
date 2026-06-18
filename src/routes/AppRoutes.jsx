ce49574517b07ee0da1c75f7c75d506f8dc1803b
Charles <charlesngatia.cn@gmail.com>
2026-06-18 21:00:58 +0300

    Fixing routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,17 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+
 import BrowseGames from "../pages/BrowseGames/BrowseGames";
-
-<Route path="/games" element={<BrowseGames />} />
-
+import About from "../pages/About/About";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/games" element={<h1>Games</h1>} />
+      <Route path="/" element={<BrowseGames />} />
+      <Route path="/games" element={<BrowseGames />} />
       <Route path="/games/:id" element={<h1>Game Details</h1>} />
       <Route path="/favorites" element={<h1>Favorites</h1>} />
-      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }

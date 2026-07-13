b025d1235f00868d554b205709873cb5e5faf690
Cwabera <charlesngatia.cn@gmail.com>
2026-07-13 21:18:12 +0300

    Update run.py

--- server/run.py
+++ server/run.py
@@ -8,6 +8,8 @@ print("SECRET_KEY =", app.config["SECRET_KEY"])
 print("JWT_SECRET_KEY =", app.config["JWT_SECRET_KEY"])
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 
